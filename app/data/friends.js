b5cb403b95bf5d542ae49c0f79d3f49f91fea8fd
ryanoliversotto <ryan.sotto@outlook.com>
2018-06-18 09:33:32 -0500

    Deploy to Heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 var friends = [
 	
 	{
-		name: "Zachary Keeshin",
-		photo: "https://scontent.ford1-1.fna.fbcdn.net/v/t1.0-9/17190956_1686211724727533_539067892412082016_n.jpg?oh=0b55db7b8fc8368da8942736eac763f2&oe=59DBA741",
+		name: "Benjamin Browder",
+		photo: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
 		scores: [
 			"5",
 			"1",
@@ -17,8 +17,8 @@ var friends = [
 		]
 	},
 	{
-		name: "Penny Keeshin",
-		photo: "https://scontent.ford1-1.fna.fbcdn.net/v/t1.0-9/12733450_10207890077428131_71717918753594055_n.jpg?oh=5a3b8de5ab0af58d176cf9b485876042&oe=59DE48D1",
+		name: "Vladamir VanDamme",
+		photo: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
 		scores: [
 			"3",
 			"1",
@@ -33,8 +33,8 @@ var friends = [
 		]
 	},
 	{
-		name: "Brett Keeshin",
-		photo: "https://scontent.ford1-1.fna.fbcdn.net/v/t1.0-9/556565_397318307001982_446614727_n.jpg?oh=ecca2cfb2ef048069d0f4c1abb6d3d52&oe=59E168E9",
+		name: "Brian Eastman",
+		photo: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
 		score: [
 			"3",
 			"1",
